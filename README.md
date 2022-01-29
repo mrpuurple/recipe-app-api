736484a68490c4924d294ec0721a22d6f1ec7836
Nichlas Handstedt <nichlas.handstedt@scania.com>
2022-01-29 18:50:16 +0100

    Added custom user model.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # recipe-app-api
-Recipe app api source code
+
+> Recipe app api source code
+
+## Create an alias for running tests
+
+```shell
+# add to .zshenv
+alias testme='docker-compose run app sh -c "python manage.py test"'
+```
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'testmail@mailservice.com'
+        password = 'Secret1234!'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'testuser@ServiceMail.COM'
+        user = get_user_model().objects.create_user(email, 'test1234')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'admin@mailservice.com',
+            'secret1234!'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
