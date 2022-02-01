ebba680e4a422fcfa5386e375ce391f5d354a41c
Nichlas Handstedt <nichlas.handstedt@scania.com>
2022-02-01 06:13:30 +0100

    fix flake8 errors

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,7 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, 
+class TagViewSet(viewsets.GenericViewSet,
                  mixins.ListModelMixin,
                  mixins.CreateModelMixin):
     """Manage tags in the database"""
@@ -20,7 +20,6 @@ class TagViewSet(viewsets.GenericViewSet,
         """Return objects for the current authenticated user only"""
 
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
 
     def perform_create(self, serializer):
         """Create a new tag"""
